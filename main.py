474a632b42e487815f07e8c876ebc1bb7fb4a3e4
Anshika <anshika@kanchans-MacBook-Pro.local>
2024-08-15 01:35:18 +0530

    flask app

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+    # return 'Hello, World!'
+
+# routing
+@app.route('/products2')
+def products():
+    return 'I am product page'
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
